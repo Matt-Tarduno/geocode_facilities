3e59886e72c2d88978a82e869c6bd3a00bc728cf
Matt-Tarduno <matthew.tarduno@gmail.com>
2017-05-23 10:47:57 -0700

    Update geocode_facilities.py

--- geocode_facilities.py
+++ geocode_facilities.py
@@ -1,21 +1,24 @@
-#setup
+#setup from mac terminal (assuming pip/virtualenv)
 # $virtualenv .
 # $virtualenv venv --distribute
 # $source venv/bin/activate
 # $pip install requests[security]
 # $pip install geocoder
 # $pip install ratelimit
+# move to working directory that houses csv ($cd Desktop/geocode3)
 # $python geocode_facilities.py
+
+#Note: May have to run code in chunks due to daily 2500 api call limit
 
 import geocoder
 import csv
 import time
 from ratelimit import rate_limited
 
-@rate_limited(5) #this package limits the rate of calls to google's API
+@rate_limited(0.5) #this package limits the rate of calls to google's API
 def do():
 	output=[]
-	with open("facilities.csv", "rU") as file:
+	with open("facilities_2.csv", "rU") as file:
 		reader = csv.reader(file)
 		facilities_list = list(reader)
 	for facility in facilities_list:
@@ -25,6 +28,9 @@ def do():
 		city=facility[1]+", "+facility[2]+", "+facility[3]
 		#default is to use penetentiary address, not city address
 		city_indicator=0
+
+		#pass through unique id
+		unique_id=facility[4]
 		
 		#Make call to googl api
 		g = geocoder.google(address, timeout=10.0)
@@ -32,12 +38,14 @@ def do():
 		#This if statement handles failures
 		#failures caused by exceeding rate limit / messy address names
 		if g.latlng==[]:
+			time.sleep(1)
 			g = geocoder.google(city, timeout=10.0)
 			if g.latlng==[]:
 				lat, lng = "none", "none"
-				#print "failed: ",
-				#print address,
-				#print g.latlng
+				print "failed: ",
+				print address,
+				print g.latlng
+				time.sleep(2)
 			else:
 				city_indicator=1
 				lat=g.latlng[0]
@@ -45,7 +53,8 @@ def do():
 		else:
 			lat=g.latlng[0]
 			lng=g.latlng[1]
-		output.append([address, lat, lng, city_indicator])
+		output.append([address, lat, lng, city_indicator, unique_id])
+		print([address, lat, lng, city_indicator, unique_id])
 
 	with open("facility_locations.csv", "wb") as f:
 		writer = csv.writer(f)
@@ -53,4 +62,5 @@ def do():
 
 if __name__ == '__main__':
     do()
+
 
